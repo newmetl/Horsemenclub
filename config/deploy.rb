be0ca837e6a027c084ca580bfddae3b4e708f0de
Wojtek Gorecki <adalbert.gorecki@gmail.com>
2012-03-13 23:26:22 +0100

    capified

---
+++ config/deploy.rb
@@ -0,0 +1,30 @@
+require 'bundler/capistrano'
+
+set :application, "Horsemenclub"
+set :domain,      "horsemenclub.com"
+set :user,        "hmc"
+set :repository,  "git@github.com/newmetl:Horsemenclub.git"
+
+set :rails_env,   "production"
+
+set :branch, "master" unless exists?(:branch)
+
+set :deploy_to,   "/home/#{user}/#{rails_env}"
+set :use_sudo,    false
+
+ssh_options[:forward_agent] = true
+set :scm, :git
+set :deploy_via, :copy
+set :git_enable_submodules, 1
+
+role :app, domain
+role :web, domain
+role :db,  domain, :primary => true
+
+namespace :deploy do
+  task(:start,   :roles => :app) {}
+  task(:stop,    :roles => :app) {}
+  task(:restart, :roles => :app, :except => { :no_release => true }) {
+    run "touch #{File.join(current_path,'tmp','restart.txt')}"
+  }
+end
