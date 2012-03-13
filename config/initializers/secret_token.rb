c5f63f5daf0e352eb6bb2d0f0c47b6dd55e01107
Wojtek Gorecki <adalbert.gorecki@gmail.com>
2012-03-13 22:54:21 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Horsemenclub::Application.config.secret_token = 'f119995042525aa168e4258bca9df5ee6836664f094b243fb3261e5fb857419c0e3cc33d1f89c48521db02069a6f1629e33d780c732430a4d14b7732a24a0d1d'
